e9ce983f31e1955f8f3b42fcdeb760cd4b0b801b
austinogiza <44683018+austinogiza@users.noreply.github.com>
2020-08-05 13:37:38 +0100

    remove heroku

--- phenommie/wsgi.py
+++ phenommie/wsgi.py
@@ -4,10 +4,10 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # for whitenoise, remove in development
-from whitenoise.django import DjangoWhiteNoise
+# from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'phenommie.settings')
 
 application = get_wsgi_application()
-# for whitenoise, remove in development
-application = DjangoWhiteNoise(application)
+# # for whitenoise, remove in development
+# application = DjangoWhiteNoise(application)
