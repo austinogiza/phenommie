9b9f2748550f622f0aa0b6c9f3914ce022e6615b
austinogiza <44683018+austinogiza@users.noreply.github.com>
2020-08-05 11:57:03 +0100

    sqllite

--- phenommie/settings.py
+++ phenommie/settings.py
@@ -72,17 +72,23 @@ WSGI_APPLICATION = 'phenommie.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'phenommie',
+#         'USER': 'root',
+#         'HOST': '',
+#         'PORT': '',
+#         'PASSWORD': ''
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'phenommie',
-        'USER': 'root',
-        'HOST': '',
-        'PORT': '',
-        'PASSWORD': ''
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
